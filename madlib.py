401d4860224ee8c73834e8853206ee07e264fb59
Neha <144268566+nehasmhs@users.noreply.github.com>
2024-09-01 22:07:38 -0500

    String_concatenation_program
    string_concatenation_program

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# String concatenation or  combining the strings together
+
+youtube_channel = " "
+
+noun = input("name: ")
+verb = input("verb: ")
+adj = input("adj: ")
+
+
+#print("I love myself." + youtube_channel)
+#print("I love myself. {}".format(youtube_channel))
+print(f"I love myself. I would like to learn {noun} really well . I am {adj} that i will {verb} it")
+
+
